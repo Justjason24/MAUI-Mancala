a5c98cbb2fddd475325aaad0088a32cc12df7688
Jason Young <Justjason24@gmail.com>
2026-02-11 22:36:47 -0600

    I got a pebble moving!

---
+++ Mancala/Mancala/CircleDrawable.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mancala
+{
+    public class CircleDrawable : IDrawable
+    {
+        public void Draw(ICanvas canvas, RectF dirtyRect)
+        {
+            canvas.FillColor = Colors.Black;
+            canvas.FillCircle(dirtyRect.Center.X, dirtyRect.Center.Y, 10);
+        }
+    }
+}
--- Mancala/Mancala/MancalaPage.xaml.cs
+++ Mancala/Mancala/MancalaPage.xaml.cs
@@ -1,3 +1,4 @@
+
 namespace Mancala;
 
 public partial class MancalaPage : ContentPage
@@ -13,5 +14,24 @@ public partial class MancalaPage : ContentPage
         // Navigate to the specified URL in the system browser.
         await Launcher.Default.OpenAsync("https://espn.com");
     }
+
+    private async void Pit51_Tapped(object sender, EventArgs e)
+    {
+        // Move the pebble from Pit51 (row 5, col 1) to Pit41 (row 4, col 1)
+        Pebble.TranslationX = 0;
+        Pebble.TranslationY = 0;
+
+        // Animate upward by one row (approx 70px depending on spacing)
+        await Pebble.TranslateTo(0, -70, 400, Easing.CubicInOut);
+
+        // Snap pebble into Pit41 grid cell
+        Grid.SetRow(Pebble, 4);
+        Grid.SetColumn(Pebble, 1);
+
+        // Reset translation after moving grid cell
+        Pebble.TranslationX = 0;
+        Pebble.TranslationY = 0;
+    }
+
 
 }
