fd1f491fce9389106fc3d8c4b498ba6a033f1bb5
Jason Young <Justjason24@gmail.com>
2026-02-26 09:39:41 -0600

    All pits are now being drawn using a GraphicsView now

--- Mancala/Mancala/MancalaPage.xaml.cs
+++ Mancala/Mancala/MancalaPage.xaml.cs
@@ -48,8 +48,8 @@ public partial class MancalaPage : ContentPage
         }
 
 
-        ((PitDrawable)Pit00View.Drawable).PebbleCount = 7;
-        Pit00View.Invalidate();
+        ((PitDrawable)Pit00.Drawable).PebbleCount = 7;
+        Pit00.Invalidate();
         Console.WriteLine();
 
         //Block.TranslationX = 0;
